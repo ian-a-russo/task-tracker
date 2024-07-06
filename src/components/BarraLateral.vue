32857c147b834a6d706165f8866fcfc1d4a2ec76
Ian Russo <ianrusso2023@gmail.com>
2024-07-06 08:47:52 -0400

    first commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,61 @@
+<template>
+  <header>
+    <h1>
+      <img src="../assets/logo.png" alt="" class="imagem" />
+    </h1>
+    <button class="button" @click="alterarTema">{{ textoBotao }}</button>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BarraLateral",
+  emits: ["aoTemaAlterado"],
+  data() {
+    return {
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
+        return "Desativar modo escuro";
+      }
+      return "Ativar modo escuro";
+    },
+  },
+  methods: {
+    alterarTema() {
+      this.modoEscuroAtivo = !this.modoEscuroAtivo;
+      this.$emit("aoTemaAlterado", this.modoEscuroAtivo);
+    },
+  },
+});
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #202160;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+.imagem {
+  border-radius: 20px;
+  margin: 10px;
+}
+.button {
+  background-color: rgba(141, 175, 238, 0.752);
+  border: #202160;
+  margin: 10px;
+}
+</style>
---
+++ src/components/TarefaTracker.vue
@@ -0,0 +1,31 @@
+<template>
+  <BoxLista>
+    <div class="column is-7">
+      {{ tarefa.descricao || "Tarefa sem descrição" }}
+    </div>
+    <div class="column">
+      <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
+    </div>
+  </BoxLista>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Cronometro from "./CronometroTracker.vue";
+import ITarefa from "../interfaces/ITarefa";
+import BoxLista from "./BoxLista.vue";
+
+export default defineComponent({
+  name: "TarefaTracker",
+  components: {
+    Cronometro,
+    BoxLista,
+  },
+  props: {
+    tarefa: {
+      type: Object as PropType<ITarefa>,
+      required: true,
+    },
+  },
+});
+</script>
